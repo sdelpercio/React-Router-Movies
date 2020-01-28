7c9d6577e26ed3950357ea31c6ac1eee14c0edb4
Shawn DelPercio <sdelperc@gmail.com>
2020-01-28 17:48:37 -0500

    added movecard stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,24 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   // Setting up state for individual movie
   const [movie, setMovie] = useState();
-  console.log("these are the props", props);
 
   // Finding correct movieID 
   const { movieID } = useParams();
-  // console.log("here is the dynamic route", movieID);
-  // const aMovie = props.movies.find(item => item.id === Number(movieID));
-  
-  // console.log("aMovie", aMovie.id);
 
   // Getting data for individual movie
   useEffect(() => {
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
-
        axios
         .get(`http://localhost:5000/api/movies/${movieID}`)
         .then(response => {
@@ -41,27 +34,8 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    <MovieCard movie={movie} />
   );
 }
 
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
-const MovieCard = props => {
+const MovieCard = ({movie}) => {
+  // Destructuring props
+  const { title, director, metascore, stars } = movie;
+
   return (
-    <div></div>
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
   );
 };
 
