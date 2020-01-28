0f576621fc8ff91c7386fd4f68ab5986fa142a6f
Shawn DelPercio <sdelperc@gmail.com>
2020-01-28 17:40:28 -0500

    added home button functionality

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
+const SavedList = props => {
+  // Setting up function to return to home page
+  const history = useHistory();
+  function goHome() {
+    history.push('/');
+  }
+
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+        {props.list.map(movie => (
+          <span className="saved-movie">{movie.title}</span>
+        ))}
+    <div onClick={goHome} className="home-button">Home</div>
   </div>
-);
+  );
+  
+};
 
 export default SavedList;
